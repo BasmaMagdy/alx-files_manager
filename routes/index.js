0e9a16f3b3b9d8baea4ee11a2d5658be69415924
Basma Magdy <basma.magdy998@gmail.com>
2024-05-17 08:46:37 -0700

    go

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,69 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+const express = require("express");
+import AppController from "../controllers/AppController";
+import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
+import FilesController from "../controllers/FilesController";
 
-function controllerRouting(app) {
-  const router = express.Router();
-  app.use('/', router);
+const router = express.Router();
 
-  // App Controller
+// GET status
+router.get("/status", (req, res) => {
+  AppController.getStatus(req, res);
+});
 
-  // should return if Redis is alive and if the DB is alive
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+//GET stats
+router.get("/stats", (req, res) => {
+  AppController.getStats(req, res);
+});
 
-  // should return the number of users and files in DB
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
+//POST uSERS
+router.post("/users", (req, res) => {
+  UsersController.postNew(req, res);
+});
 
-  // User Controller
+//GET connect
+router.get("/connect", (req, res) => {
+  AuthController.getConnect(req, res);
+});
 
-  // should create a new user in DB
-  router.post('/users', (req, res) => {
-    UsersController.postNew(req, res);
-  });
+//GET disconnect
+router.get("/disconnect", (req, res) => {
+  AuthController.getDisconnect(req, res);
+});
 
-  // should retrieve the user base on the token used
-  router.get('/users/me', (req, res) => {
-    UsersController.getMe(req, res);
-  });
+//GET users/me
+router.get("/users/me", (req, res) => {
+  UsersController.getMe(req, res);
+});
 
-  // Auth Controller
+//POST /files
+router.post("/files", (req, res) => {
+  FilesController.postUpload(req, res);
+});
 
-  // should sign-in the user by generating a new authentication token
-  router.get('/connect', (req, res) => {
-    AuthController.getConnect(req, res);
-  });
+//GET /files/:id
+router.get("/files/:id", (req, res) => {
+  FilesController.getShow(req, res);
+});
 
-  // should sign-out the user based on the token
-  router.get('/disconnect', (req, res) => {
-    AuthController.getDisconnect(req, res);
-  });
+//GET /files
+router.get("/files", (req, res) => {
+  FilesController.getIndex(req, res);
+});
 
-  // Files Controller
+//PUT /files/:id/publish
+router.put("/files/:id/publish", (req, res) => {
+  FilesController.putPublish(req, res);
+});
 
-  // should create a new file in DB and in disk
-  router.post('/files', (req, res) => {
-    FilesController.postUpload(req, res);
-  });
+//PUT /files/:id/unpublish
+router.put("/files/:id/unpublish", (req, res) => {
+  FilesController.putUnpublish(req, res);
+});
 
-  // should retrieve the file document based on the ID
-  router.get('/files/:id', (req, res) => {
-    FilesController.getShow(req, res);
-  });
+//GET /files/:id/data
+router.get("/files/:id/data", (req, res) => {
+  FilesController.getFile(req, res);
+});
 
-  // should retrieve all users file documents for a
-  // specific parentId and with pagination
-  router.get('/files', (req, res) => {
-    FilesController.getIndex(req, res);
-  });
-
-  // should set isPublic to true on the file document based on the ID
-  router.put('/files/:id/publish', (req, res) => {
-    FilesController.putPublish(req, res);
-  });
-
-  // should set isPublic to false on the file document based on the ID
-  router.put('/files/:id/unpublish', (req, res) => {
-    FilesController.putUnpublish(req, res);
-  });
-
-  // should return the content of the file document based on the ID
-  router.get('/files/:id/data', (req, res) => {
-    FilesController.getFile(req, res);
-  });
-}
-
-export default controllerRouting;
+module.exports = router;
