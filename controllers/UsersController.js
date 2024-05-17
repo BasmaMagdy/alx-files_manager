ea80145b7a62c71bd8b774699d32403ecce1c392
Basma Magdy <basma.magdy998@gmail.com>
2024-05-17 09:10:19 -0700

    go

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,31 +1,59 @@
-import Queue from 'bull';
-import UsersCollection from '../utils/users';
-
-// User welcome email queue
-const userQueue = Queue('send welcome email');
+// import getUserIdAndKey from '../utils/user';
+import sha1 from "sha1";
+import { getUser } from "../utils/user";
+import utilsCollection from "../utils/collections";
+// import { ObjectId } from 'mongodb';
 
 class UsersController {
-  /**
-   * Controller for endpoint POST /users for creating new users
-   * @typedef {import("express").Request} Request
-   * @typedef {import("express").Response} Response
-   * @param {Request} req - request object
-   * @param {Response} res - response object
-   */
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (email === undefined) {
-      res.status(400).json({ error: 'Missing email' });
-    } else if (password === undefined) {
-      res.status(400).json({ error: 'Missing password' });
-    } else if (await UsersCollection.getUser({ email })) {
-      res.status(400).json({ error: 'Already exist' });
-    } else {
-      // Create new user
-      const userId = await UsersCollection.createUser(email, password);
-      userQueue.add({ userId });
-      res.status(201).json({ id: userId, email });
+    if (!email) {
+      return res.status(400).send({ error: "Missing email" });
     }
+
+    if (!password) {
+      return res.status(400).send({ error: "Missing password" });
+    }
+
+    try {
+      const usersCollection = await utilsCollection("users");
+
+      // Check if user already exists
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).send({ error: "Already exist" });
+      }
+      const hashedPassword = sha1(password);
+      const newUser = await usersCollection.insertOne({
+        email,
+        password: hashedPassword,
+      });
+      const userId = newUser.insertedId;
+      res.status(201).send({ id: userId, email });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async getMe(req, res) {
+    const user = await getUser(req);
+    console.log(`==================> ${user}`);
+    if (!user) {
+      return res.status(401).send({ error: "Unauthorized" });
+    }
+    res.status(201).send({ id: user._id, email: user.email });
+    // const { userId } = await getUserIdAndKey(req);
+
+    // if (!userId) {
+    //   return res.status(401).send(`no user found in this id ${userId}`);
+    // }
+    // const usersCollection = await utilsCollection('users');
+
+    // const { email } = await usersCollection.findOne({ _id: ObjectId(userId) });
+    // if (!email) {
+    //   return res.status(401).send('Unauthorized');
+    // }
+    // res.status(201).send({ id: userId, email });
   }
 }
 
