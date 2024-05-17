0704788ce6f92361e1e8605a8999040ff34394b5
Basma Magdy <basma.magdy998@gmail.com>
2024-05-17 08:48:00 -0700

    go

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-import express from 'express';
-import router from './routes/index';
-import unmatchedRouteHandler from './middleware/unmatched';
-import errorHandler from './middleware/error';
-import shutdown from './utils/shutdown';
+const express = require("express");
+const indexRouter = require("./routes/index");
 
-// Express server
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-app.use(unmatchedRouteHandler);
-app.use(errorHandler);
 
-const server = app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
+app.use(indexRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is running in port ${port}`);
 });
-
-// Graceful shutdown
-const handler = () => shutdown(server);
-process.on('SIGINT', handler);
-process.on('SIGTERM', handler);
-process.on('SIGQUIT', handler);
-
-export default app;
