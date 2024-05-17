e4147ba021a2a02b59d08faf0f5fad9334ef5036
Basma Magdy <basma.magdy998@gmail.com>
2024-05-17 09:16:02 -0700

    go

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,68 +1,48 @@
-import UsersCollection from '../utils/users';
-import AuthTokenHandler from '../utils/tokens';
-import PasswordHandler from '../utils/passwords';
+import { createHash } from "crypto";
+import { getUserIdAndKey } from "../utils/user";
+import { v4 as uuidv4 } from "uuid";
+import dbClient from "../utils/db";
+import redisClient from "../utils/redis";
 
 class AuthController {
-  /**
-   * Controller for GET /connect endpoint for authorizing users
-   * using Basic Auth scheme
-   * @param {import("express").Request} req - request object
-   * @param {import("express").Response} res - response object
-   */
   static async getConnect(req, res) {
-    // Get authorization parameters
-    const authParams = req.get('Authorization');
-    if (!authParams) return res.status(401).json({ error: 'Unauthorized' });
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Basic ")) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const base64Credentials = authHeader.split(" ")[1];
+    const credentials = Buffer.from(base64Credentials, "base64").toString();
+    const [email, password] = credentials.split(":");
 
-    // Get base64 authentication parameters and decrypt to ascii
-    const credentials = Buffer
-      .from(authParams.replace('Basic', ''), 'base64')
-      .toString('ascii')
-      .split(':');
-    const email = credentials[0] || '';
-    const password = credentials[1] || '';
-
-    // Check if user exists
-    const user = await UsersCollection.getUser({ email });
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
+    if (!email || !password) {
+      return res.status(401).json({ error: "Unauthorized" });
     }
 
-    // Check if passwords match
-    if (!PasswordHandler.isPasswordValid(password, user.password)) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
+    const hashedPassword = createHash("sha1").update(password).digest("hex");
+    try {
+      const user = await dbClient.getUserByEmail(email);
 
-    const token = await AuthTokenHandler.createAuthToken(user);
-    return res.status(200).json({ token });
+      if (!user || user.password !== hashedPassword) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      const token = uuidv4();
+      const key = `auth_${token}`;
+      await redisClient.set(key, user._id.toString(), 86400);
+      return res.status(200).send({ token });
+    } catch (error) {
+      console.error("Error signing in:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 
-  /**
-   * Controller for GET /disconnect endpoint that logs out user
-   * if they were logged in.
-   * @param {import("express").Request} req - request object
-   * @param {import("express").Response} res - response object
-   */
   static async getDisconnect(req, res) {
-    const token = req.get('X-Token');
-    if (!await AuthTokenHandler.getUserByToken(token)) {
-      res.status(401).json({ error: 'Unauthorized' });
-      return;
+    const user = await getUserIdAndKey(req);
+    if (!user) {
+      return res.status(401).send("Unauthorized");
     }
-    await AuthTokenHandler.deleteAuthToken(token);
-    res.status(204).json();
-  }
-
-  /**
-   * Controller for GET /users/me endpoint that retrieves information
-   * about a logged in user
-   * @param {import("express").Request} req - request object
-   * @param {import("express").Response} res - response object
-   */
-  static async getMe(req, res) {
-    const { user } = req;
-    if (!user) res.status(401).json({ error: 'Unauthorized' });
-    else res.status(200).json({ id: user._id.toString(), email: user.email });
+    await redisClient.del(user.key);
+    res.status(204).send();
   }
 }
 
